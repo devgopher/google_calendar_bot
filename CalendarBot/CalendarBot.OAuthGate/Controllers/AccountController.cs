6627988b47b482b7f9165a293c8b2ea5a998a9f1
Igor Evdokimov <igor.evdokimov@external.tele2.ru>
2025-08-15 18:07:05 +0300

    - google oauth gate to redirect after auth to get token

---
+++ CalendarBot/CalendarBot.OAuthGate/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using CalendarBot.OAuthGate.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CalendarBot.OAuthGate.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IOptionsSnapshot<GoogleAuthSettings> _settings;
+
+    public AccountController(IOptionsSnapshot<GoogleAuthSettings> settings)
+    {
+        _settings = settings;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GoogleResponse(string returnUrl = "/")
+    {
+        var result = await HttpContext.AuthenticateAsync("Cookies");
+        var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+        return Redirect(returnUrl); // Перенаправление на исходный URL
+    }
+
+    public IActionResult Logout() => SignOut("Cookies", GoogleDefaults.AuthenticationScheme);
+}
---
+++ CalendarBot/CalendarBot.OAuthGate/Settings/GoogleAuthSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CalendarBot.OAuthGate.Settings;
+
+public class GoogleAuthSettings
+{
+    public const string Section = "GoogleAuth";
+    public required string ClientId { get; set; }
+    public required string ClientSecret { get; set; }
+}
